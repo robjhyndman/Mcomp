f5ee130dbc8beb7e36ab842063d50336ef1c4b43
Rob J Hyndman <robjhyndman@gmail.com>
2025-12-10 15:11:06 +1100

    Removed old test

--- tests/testthat/test_Mcomp.R
+++ tests/testthat/test_Mcomp.R
@@ -4,7 +4,6 @@ test_that("plot and autoplot return the right classes", {
   expect_equal(plot(M1[[1]]), NULL)
   expect_is(autoplot(M3[[1]]), "gg")
   expect_is(autoplot(M3[[1]]), "ggplot")
-  expect_equal(length(autoplot(M3[[1]])), 11)
 })
 
 test_that("subset returns the right number of series using examples in help file", {
